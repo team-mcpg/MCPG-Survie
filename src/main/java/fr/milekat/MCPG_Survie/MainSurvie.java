36aa92fa63dcffa0d12cf6d50a56970125e94fa9
tutur1004 <arthur.barthelme78@gmail.com>
2021-04-08 18:15:59 +0200

    Initial commit

---
+++ src/main/java/fr/milekat/MCPG_Survie/MainSurvie.java
@@ -0,0 +1,10 @@
+package fr.milekat.MCPG_Survie;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MainSurvie extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        
+    }
+}
